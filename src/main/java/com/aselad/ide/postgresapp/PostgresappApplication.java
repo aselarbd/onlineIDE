f31f6ce1cf5c09d9ee0060a804ea9cc30b35043f
Asela Dasanayaka <aselarbd@gmail.com>
2019-11-23 16:43:22 +0100

    added Log4j logger

--- src/main/java/com/aselad/ide/postgresapp/PostgresappApplication.java
+++ src/main/java/com/aselad/ide/postgresapp/PostgresappApplication.java
@@ -1,13 +1,22 @@
 package com.aselad.ide.postgresapp;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PostgresappApplication {
+public class PostgresappApplication implements CommandLineRunner {
+
+    private static final Logger LOGGER = LogManager.getLogger(PostgresappApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(PostgresappApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
