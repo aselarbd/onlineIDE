1f1071731dbf93e31da56978d14fec6a9c9152b1
Asela Dasanayaka <aselarbd@gmail.com>
2019-11-23 16:21:29 +0100

    Initial Commit

---
+++ src/test/java/com/aselad/ide/postgresapp/PostgresappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aselad.ide.postgresapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
