d839e42b5cbe874b4c2fb95c5603105311aad3ac
Asela Dasanayaka <aselarbd@gmail.com>
2019-11-25 19:04:40 +0100

    Added exception handling Issue #1

--- src/main/java/com/aselad/ide/postgresapp/Constants.java
+++ src/main/java/com/aselad/ide/postgresapp/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
     public static final String PROJECT_API ="project";
     public static final String REGEX_FOR_NUMBERS = "^\\d+$";
     public static final String MESSAGE_FOR_REGEX_NUMBER_MISMATCH = "ID should contains integers only";
+    public static final String DATE_FORMAT = "dd-MM-yyyy hh:mm:ss";
+    public static final String MESSAGE_FOR_INVALID_PARAMETERS_ERROR = "Invalid Parameters";
+    public static final String MESSAGE_FOR_INVALID_BODY_ERROR = "Invalid Method Body. Check JSON Objects";
 }
